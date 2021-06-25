23a04f632e9ac584baa52d4804d7309bae58c6dc
HassanBahati <mukisabahati@gmail.com>
2021-06-25 13:13:38 +0300

    added uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DATABASE_CONNECTION, {
+  await mongoose.connect("mongodb+srv://bahati:bahati@safe-courier.e9cwq.mongodb.net/safe-courier", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
