9dab7829a7b5affb8b3e29411884dd96ab174510
HassanBahati <mukisabahati@gmail.com>
2021-06-25 13:07:30 +0300

    edited mongouri

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api/orders", require("./routes/order"));
 // Error Handler Middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 const server = app.listen(PORT, () =>
   console.log(`Sever running on port ${PORT}`)
